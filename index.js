16fb2d12ea6e01b22a4729b74f6f409469905354
abdikafimohamud <185328738+abdikafimohamud@users.noreply.github.com>
2025-02-27 11:09:11 -0800

    review strings lab

--- index.js
+++ index.js
@@ -1,1 +1,5 @@
 // Write your code in this file!
+let welcomeMessage = "Welcome to Flatbook, 'currentUser!";
+let currentUser = 'currentUser'
+let excitedWelcomeMessage = "WELCOME TO FLATBOOK, 'CURRENTUSER'!";
+const shortGreeting = "Welcome, User!";
